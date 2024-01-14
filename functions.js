e436d8577b86de9ea1ccd8757198841a58f053e5
wankunda sikaonga <sikaongawankunda@gmail.com>
2024-01-14 19:38:07 +0200

    refactored the code to make the mouse leave a trail on the divs

--- functions.js
+++ functions.js
@@ -1,14 +1,24 @@
+const btn = document.querySelector('#btn')
+let grid = 16
+btn.addEventListener('click', ()=>{
+   grid = parseInt(prompt("enter the grid dimentions "))
+   console.log(grid)
+   return grid 
+})
+
 //connecting to main div in html file 
 const main = document.querySelector('#container');
 //attributes added to main div to make the divs wrap around in order to have a grid
-main.setAttribute('style','background-color: blue;display:flex; flex-wrap:wrap; height:480px; width:480px;');
+main.style.cssText = 'display:flex; flex-wrap:wrap; margin-left:35%; height:160px; width:160px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);';
 
 //loop to create 256 identical divs 
-for(let i = 0; i < 256; i++){
+for(let i = 0; i < (grid * grid); i++){
     const div = document.createElement('div');
-    div.setAttribute('style', 'height:30px; width:30px; background-color:pink;')
-    div.addEventListener('hover', ()=>{
-        div.setAttribute('style', 'background-color:black;')
+    div.style.cssText = 'height:10px; width:10px; background-color:white;';
+
+    div.addEventListener('mouseenter', ()=>{
+        div.style.backgroundColor ='black';
     })
     main.appendChild(div)
 }
+
